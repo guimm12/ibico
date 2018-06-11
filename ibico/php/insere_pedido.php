6d332367bd3541b966ae2b52f6c180c15905d8d7
Evandro <39386199+Reignomo@users.noreply.github.com>
2018-06-10 23:45:39 -0300

    Add files via upload

--- ibico/php/insere_pedido.php
+++ ibico/php/insere_pedido.php
@@ -1,5 +1,25 @@
-<?php
+<?php 
+session_start();
+include_once("pedidos_class.php");
 
-include_once("pedido.php");
-criarPedido();
+
+	//$img = $_POST[''];
+   
+    $cd=$_SESSION['cd_usuario'];
+	$title = $_POST['Ped_title'];
+	$estado = $_POST['Ped_estado'];
+	$municipio = $_POST['Ped_municipio'];
+	$bairro = $_POST['Ped_bairro'];
+	$categoria = $_POST['Ped_categoria'];
+	$descri = $_POST['Ped_descrava'];
+
+	$obj_pedido = new pedido($title,$descri,$cd,$estado,$municipio,$bairro,$categoria);
+
+	$obj_pedido->AddPeido();
+
+
+
+
+
+
 ?>
